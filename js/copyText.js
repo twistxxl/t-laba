7211d356a8b735bcd581300e4256840fcec60301
twistxxl <glade1337@mail.ru>
2024-08-04 16:41:44 +0300

    add animation

---
+++ js/copyText.js
@@ -0,0 +1,29 @@
+
+
+
+const text  = document.getElementById('email');
+
+const textStyle = {  
+    color: '#28D979',  
+    transition: '0.5s ease',  
+    marginLeft: '10px',  
+    transform: 'scale(1.1)',  
+};  
+
+text.addEventListener('click', (e) => {
+    if(e.ctrlKey) {
+        navigator.clipboard.writeText(text.textContent);
+        
+        text.textContent = 'Copied!';
+        
+        Object.assign(text.style, textStyle);
+        
+        setTimeout(() => {
+            text.textContent = localStorage.getItem('mainEmail');
+            text.style.color = 'white';
+            text.style.transform = 'scale(1)';
+            text.style.marginLeft = '0';
+        }, 1000);
+    }
+    
+});
--- js/main.js
+++ js/main.js
@@ -1,49 +1,61 @@
 
 
+const styleForInput = {
+
+}
 
 
-document.addEventListener('DOMContentLoaded', () => {  
-    const textElements = document.querySelectorAll('.text'); 
+document.addEventListener('DOMContentLoaded', () => {
+    const textElements = document.querySelectorAll('.text');
 
 
 
-    textElements.forEach(el => {  
-        
-        const key = el.getAttribute('data-key');  
-        const savedData = localStorage.getItem(key);  
-        if (savedData) {  
-            el.textContent = savedData;  
-        }  
+    textElements.forEach(el => {
+
+        const key = el.getAttribute('data-key');
+        const savedData = localStorage.getItem(key);
+        if (savedData) {
+            el.textContent = savedData;
+        }
 
         //change text  
-        el.addEventListener('dblclick', () => {  
-            const currentText = el.textContent;  
-            const input = document.createElement('input');  
-            input.type = 'text';  
-            input.value = currentText;  
-            input.style.width = '100%';  
+        el.addEventListener('dblclick', () => {
+            const currentText = el.textContent;
+            const container = document.createElement('div');
+            container.id = 'containerForInput';
 
-            //text ro input
-            el.innerHTML = '';  
-            el.appendChild(input);  
-            input.focus();  
+               
+
+                //create input
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.value = currentText;
+                input.style.width = '100%';
+                container.appendChild(input);
+            //text to input
+            el.innerHTML = '';
+            el.appendChild(container);
+            input.focus();
 
             //unfocus or enter to save  
-            input.addEventListener('blur', save);  
-            input.addEventListener('keypress', (e) => {  
-                if (e.key === 'Enter') {  
-                    save();  
-                }  
-            });  
+            input.addEventListener('blur', save);
+            input.addEventListener('keypress', (e) => {
+                if (e.key === 'Enter') {
+                    save();
+                }
+            });
 
-            function save() {  
-                const newData = input.value.trim();  
-                el.textContent = newData;  
-
-                //save to local storage
-                localStorage.setItem(key, newData);  
-            }  
-        });  
-    });  
-});  
+            function save() {
+                const newData = input.value.trim();
+                if(newData) {
+                    el.textContent = newData;
+                    localStorage.setItem(key, newData);
+                } else {
+                    el.textContent = currentText;
+                }
+                
+            }
+        });
+    });
+});
 
